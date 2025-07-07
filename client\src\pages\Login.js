7d3b6a8f1dd4a82a26f7c423b34e51018543ae63
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-07 17:40:59 +0500

    Add client\src\pages\Login.js

---
+++ client\src\pages\Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/api/login', { email, password });
+      localStorage.setItem('token', response.data.token);
+      window.location.href = '/';
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Email:</label>
+        <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+        <br />
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
