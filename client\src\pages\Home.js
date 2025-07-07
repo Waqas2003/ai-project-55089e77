b1d58ede79e05835b9d622acfb73c256067d9734
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-07 17:40:57 +0500

    Add client\src\pages\Home.js

---
+++ client\src\pages\Home.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post._id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
